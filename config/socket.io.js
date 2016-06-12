1df8f1b80b1dcc716fa8e3fbc9ee2b3228153d31
emeson <emesonsantana@gmail.com>
2016-06-12 14:47:59 -0300

    improved authentication

--- config/socket.io.js
+++ config/socket.io.js
@@ -1,6 +1,7 @@
 var socketIO = require('socket.io');
+var redisClient = require('redis');
+var url = require('url');
 var glob = require('glob');
-var redis = require('redis');
 var logger = require('winston');
 
 module.exports = function (server, config) {
@@ -8,8 +9,8 @@ module.exports = function (server, config) {
   var authentication = require(config.root + '/app/helpers/authentication.js');
   var manager = require(config.root + '/app/helpers/manager.js');
 
-  var redisClient = redis.createClient(config.db);
-  redisClient.subscribe('server-message');
+  var redis = redisClient.createClient(config.db);
+  redis.subscribe('server-message');
   /*
    * Server Message Structure
    * message = {
@@ -24,7 +25,7 @@ module.exports = function (server, config) {
    *  }
    */
 
-  var socketServer = socketIO(server, config.socket.options);
+  var io = socketIO(server, config.socket.options);
 
   var list = [];
   /*
@@ -37,37 +38,62 @@ module.exports = function (server, config) {
   });
 
   /*
+   * Socket Authentication
+   */
+  io.use(function(socket, next){
+
+    var requestUrl = url.parse(socket.request.url);
+    var requestQuery = requestUrl.query;
+    var requestParams = requestQuery.split("&");
+
+    var params = {};
+    for (var i=0;i<requestParams.length;i++) {
+      var pair = requestParams[i].split("=");
+          // If first entry with this name
+      if (typeof params[pair[0]] === "undefined") {
+        params[pair[0]] = decodeURIComponent(pair[1]);
+          // If second entry with this name
+      } else if (typeof params[pair[0]] === "string") {
+        var arr = [ params[pair[0]],decodeURIComponent(pair[1]) ];
+        params[pair[0]] = arr;
+          // If third or later entry with this name
+      } else {
+        params[pair[0]].push(decodeURIComponent(pair[1]));
+      }
+    }
+
+    logger.info('new request', {request_url: socket.request.url, authorization: params.authorization, timestamp: Date.now(), pid: process.pid});
+
+    authentication.login(params.authorization).then(function(user){
+      logger.info('authenticated', {request_url: socket.request.url, authorization: params.authorization, user: user, timestamp: Date.now(), pid: process.pid});
+      socket.uid = user.uid;
+      socket.user = user;
+      next();
+    }, function () {
+      logger.info('unauthorized', {request_url: socket.request.url, authorization: params.authorization, timestamp: Date.now(), pid: process.pid});
+      next(new Error('Unauthorized'));
+    });
+  });
+
+  /*
    * Init Socket Server
    */
-  socketServer.on('connection', function (socket) {
-    socket.on('authentication', function (data) {
-      logger.info('new request authentication', {data: data, timestamp: Date.now(), pid: process.pid});
-      authentication.login(data).then(function(user){
-        logger.info('authenticated', {data: data, user: user, timestamp: Date.now(), pid: process.pid});
-        socket.uid = user.uid;
-        socket.user = user;
+  io.on('connection', function (socket) {
+      manager.add(socket);
 
-        manager.add(socket);
-        /*
-         * Apply Listeners
-         */
-        listeners.forEach(function (listener) {
-          listener(socket, {
-            redis: redisClient,
-            manager: manager,
-            config: config
-          });
+      /*
+       * Apply Listeners
+       */
+      listeners.forEach(function (listener) {
+        listener(socket, {
+          redis: redis,
+          manager: manager,
+          config: config
         });
-
-        socket.once('disconnect', function(){
-          authentication.logout(data);
-        });
-
-        socket.emit('authenticated');
-      }, function(){
-        logger.info('unauthorized', {data: data, timestamp: Date.now(), pid: process.pid});
-        socket.disconnect(true);
       });
-    });
+
+      socket.once('disconnect', function(){
+        authentication.logout(socket);
+      });
   });
 };
